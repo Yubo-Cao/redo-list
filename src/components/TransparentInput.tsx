7d18676d8cf52967c67d377797ed643a0a83d860
Yubo Cao <cao2006721@gmail.com>
2023-01-10 19:08:25 -0500

    feat: add transparent input

---
+++ src/components/TransparentInput.tsx
@@ -0,0 +1,59 @@
+import { cls } from "@/lib/utils";
+import { MutableRefObject, forwardRef, useState } from "react";
+
+export type TransparentInputProps = {
+    value: string;
+    onChange: (value: string) => void;
+    edit?: boolean;
+    onEdit?: (editing: boolean) => void;
+    placeholder?: string;
+    className?: string;
+    style?: React.CSSProperties;
+};
+
+export const TransparentInput: React.FC<TransparentInputProps> = forwardRef(
+    (props, ref: MutableRefObject<HTMLDivElement>) => {
+        const { value, onChange, style, className, placeholder = "" } = props;
+
+        // handle edit and fallback
+        let { edit, onEdit } = props;
+        const [_edit, _setEdit] = useState(edit);
+        onEdit = onEdit != null ? onEdit : _setEdit;
+
+        // default style
+        const clz = cls("text-light-text dark:text-dark-text", "w-full");
+
+        return (
+            <div
+                onClick={(e) => onEdit(true)}
+                onKeyDown={(e) => e.key === "Enter" && onEdit(false)}
+                style={style}
+                className={cls(className)}
+                ref={ref}
+            >
+                {_edit ? (
+                    <input
+                        type="text"
+                        className={cls(
+                            clz,
+                            "text-uim-500 dark:text-uim-400",
+                            className
+                        )}
+                        value={value}
+                        onChange={(e) => onChange(e.target.value)}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                        placeholder={placeholder}
+                        tabIndex={0}
+                        autoFocus
+                    />
+                ) : (
+                    <p className={cls(clz, "cursor-pointer", className)}>
+                        {value || placeholder}
+                    </p>
+                )}
+            </div>
+        );
+    }
+);
--- src/features/todos/TodoTitle.tsx
+++ src/features/todos/TodoTitle.tsx
@@ -1,63 +1,27 @@
-import { Todo, selectTodoById, updateTodo } from "./todosSlice";
-import { cls } from "@/lib/utils";
+import {
+    TransparentInput,
+    TransparentInputProps
+} from "@/components/TransparentInput";
 import { useAppDispatch, useAppSelector } from "@/store";
-import { forwardRef, useState } from "react";
+import { forwardRef } from "react";
+import { Todo, selectTodoById, updateTodo } from "./todosSlice";
 
 function TodoTitle(
-    {
-        id,
-        className,
-        style
-    }: {
-        id: Todo["id"];
-        className?: string;
-        style?: React.CSSProperties;
-    },
+    { id, ...rest }: { id: Todo["id"] } & TransparentInputProps,
     ref?: React.RefObject<HTMLInputElement>
 ) {
     const todo = useAppSelector((state) => selectTodoById(state, id)),
         { title } = todo,
-        [edit, setEdit] = useState(false),
         dispatch = useAppDispatch();
 
-    const clz = cls("text-light-text dark:text-dark-text", "w-full");
-
     return (
-        <div
-            onClick={(e) => setEdit(true)}
-            onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                    setEdit(false);
-                }
-            }}
-        >
-            {edit ? (
-                <input
-                    type="text"
-                    className={cls(clz, "font-black", className)}
-                    value={title}
-                    onChange={(e) => {
-                        dispatch(
-                            updateTodo({
-                                id,
-                                update: { title: e.target.value }
-                            })
-                        );
-                    }}
-                    onClick={(e) => {
-                        e.stopPropagation();
-                    }}
-                    placeholder="Title of the task"
-                    ref={ref}
-                    tabIndex={0}
-                    autoFocus
-                />
-            ) : (
-                <p className={cls(clz, "cursor-pointer", className)}>
-                    {title || "Title of the task"}
-                </p>
-            )}
-        </div>
+        <TransparentInput
+            value={title}
+            onChange={(value) =>
+                dispatch(updateTodo({ id, update: { title: value } }))
+            }
+            {...rest}
+        />
     );
 }
 
